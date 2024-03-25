ec025e3b0b70843a737320bc767e19902400c1e6
jofetta <jafarovatatiana@gmail.com>
2024-03-25 15:11:10 +0300

    feat: solve 2 more tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -129,8 +129,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateToCheck = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+  return dateToCheck >= startDate && dateToCheck <= endDate;
 }
 
 /**
@@ -144,8 +147,9 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateToFormat = new Date(new Date(date).getTime() - 3 * 3600000);
+  return dateToFormat.toLocaleString('en-US');
 }
 
 /**
